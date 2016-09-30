99d0a3d2c82fcbfb8eeb541838f34daf0744d526
Nicola <lynxnic99@gmail.com>
2016-09-30 18:20:57 +0200

    base class

---
+++ SUDOKU.java
@@ -0,0 +1,20 @@
+/*
+ * SUDOKU MAIN CLASS
+ */
+package sudoku;
+
+/**
+ *
+ * @author Lince99
+ * 
+ */
+public class SUDOKU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application down here, we hope this java application doesn't have SO MUCH BUGS
+    }
+    
+}
