3f95b967d06a8fa7379aa7c944ae7802d4156194
Oples <fornasat@gmail.com>
2016-09-30 23:06:59 +0200

    Help msgbox

--- SUDOKU.java
+++ SUDOKU.java
@@ -1,12 +1,16 @@
 /*
+ * Imports
+ */
+import java.lang.String;
+/*
  * SUDOKU MAIN CLASS
  */
 package sudoku;
 
 /**
- *
  * @author Lince99
- * 
+ * @author Oples
+ * @author Marxist Carbonara
  */
 public class SUDOKU {
 
@@ -15,6 +19,33 @@ public class SUDOKU {
      */
     public static void main(String[] args) {
         // TODO code application down here, we hope this java application doesn't have SO MUCH BUGS
-    }
-    
+		try{
+			String options = args[0];
+			if (options.startsWith("-"))
+			{
+				if(options == "-h")
+				{
+					System.out.println(help());
+				}
+			}
+			else
+			{
+				System.out.println("Don't fuck with commands plz");
+				// TODO: improve savages way to insult testers :3
+			}
+		}
+		catch (Exception e)
+		{
+				System.out.println(help());
+		}
+	}
+   public static String help(){
+      String msgbox = new String();
+      msgbox  = "################################\n";
+      msgbox += "#  HELP REQUESTED! (help menu) #\n";
+      msgbox += "#                              #\n";
+      msgbox += "################################\n";
+      
+      return msgbox; 
+   }
 }
