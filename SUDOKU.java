4975be25f3490a12775e7a5485c3ba9d6e7d1621
Oples <fornasat@gmail.com>
2016-10-02 00:11:29 +0200

    setting up commands for updates

--- SUDOKU.java
+++ SUDOKU.java
@@ -2,7 +2,8 @@
  * Imports
  */
 import java.lang.String;
-import Classes.Updater;
+import java.io.*;
+//import Classes.Updater;
 /*
  * SUDOKU MAIN CLASS
  */
@@ -21,6 +22,8 @@ public class SUDOKU {
     public static void main(String[] args) 
     {
         // TODO code application down here, we hope this java application doesn't have SO MUCH BUGS
+	Runtime rt = Runtime.getRuntime();
+	Process pr = rt.exec("echo hi");
         
 	}
 }
