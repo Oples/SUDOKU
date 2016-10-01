bc363b19617f24bbb07b58dc4945615033f3940a
Oples <fornasat@gmail.com>
2016-10-02 00:13:13 +0200

    Update SUDOKU.java

--- SUDOKU.java
+++ SUDOKU.java
@@ -22,8 +22,14 @@ public class SUDOKU {
     public static void main(String[] args) 
     {
         // TODO code application down here, we hope this java application doesn't have SO MUCH BUGS
-	Runtime rt = Runtime.getRuntime();
-	Process pr = rt.exec("echo hi");
-        
+	try
+	{
+		Runtime rt = Runtime.getRuntime();
+		Process pr = rt.exec("echo hi");
+	}
+	catch(excention IOException)
+	{
+		System.out.println("Failed to execute!");
+	}
 	}
 }
