cbbd379ff153be4d92a95a947cfd66af82f23ae9
Oples <fornasat@gmail.com>
2016-10-01 23:22:48 +0200

    Update SUDOKU.java

--- SUDOKU.java
+++ SUDOKU.java
@@ -1,20 +1,26 @@
 /*
+ * Imports
+ */
+import java.lang.String;
+
+/*
  * SUDOKU MAIN CLASS
  */
-package sudoku;
+//package sudoku;
 
 /**
- *
  * @author Lince99
- * 
+ * @author Oples
+ * @author Marxist Carbonara
  */
 public class SUDOKU {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         // TODO code application down here, we hope this java application doesn't have SO MUCH BUGS
-    }
-    
+        
+	}
 }
